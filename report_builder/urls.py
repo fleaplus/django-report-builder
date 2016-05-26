db6005a716eb88bcbcd9099e1485bbf7b40ecd05
Geoffrey Thompson <gthompson@E6540>
2016-05-26 09:23:26 -0600

    WIP Only require login to view reports.

--- report_builder/urls.py
+++ report_builder/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from django.contrib.admin.views.decorators import staff_member_required
+from django.config.auth.decorators import login_required
 from rest_framework import routers
 from . import views
 from .api import views as api_views
@@ -21,13 +21,13 @@ urlpatterns = [
     url(r'^export_to_report/$', views.ExportToReport.as_view(), name="export_to_report"),
     url(r'^api/', include(router.urls)),
     url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/related_fields', staff_member_required(api_views.RelatedFieldsView.as_view()), name="related_fields"),
-    url(r'^api/fields', staff_member_required(api_views.FieldsView.as_view()), name="fields"),
-    url(r'^api/report/(?P<report_id>\w+)/generate/', staff_member_required(api_views.GenerateReport.as_view()), name="generate_report"),
+    url(r'^api/related_fields', login_required(api_views.RelatedFieldsView.as_view()), name="related_fields"),
+    url(r'^api/fields', login_required(api_views.FieldsView.as_view()), name="fields"),
+    url(r'^api/report/(?P<report_id>\w+)/generate/', login_required(api_views.GenerateReport.as_view()), name="generate_report"),
     url('^report/(?P<pk>\d+)/$', views.ReportSPAView.as_view(), name="report_update_view"),
 ]
 
 if not hasattr(settings, 'REPORT_BUILDER_FRONTEND') or settings.REPORT_BUILDER_FRONTEND:
     urlpatterns += [
-        url(r'^', staff_member_required(views.ReportSPAView.as_view()), name="report_builder"),
+        url(r'^', login_required(views.ReportSPAView.as_view()), name="report_builder"),
     ]
